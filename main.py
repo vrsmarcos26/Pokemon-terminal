a46c98ce03138e755a5ece01fa7e72971bbc1f9c
Marcos Vinícius <98711516+vrsmarcos26@users.noreply.github.com>
2025-01-23 01:08:31 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import pickle
 from pokemon import *
 from Pessoa import *
+from colorama import init, Fore, Back, Style
 
+# Inicia a colorama
+init(autoreset=True)
 
 def escolher_pokemon_inicial(player):
-    print(f'Agora você irá poder escolher o seu Pokemon inicial que irá te acompanhar nessa jornada!')
+    print(Fore.GREEN + "Agora você irá poder escolher o seu Pokemon inicial que irá te acompanhar nessa jornada!")
 
     pikachu = PokemonEletrico('Pikachu', level=5)
     charmander = PokemonFogo('Charmander', level=5)
@@ -12,7 +15,7 @@ def escolher_pokemon_inicial(player):
     squarly = PokemonAgua('Squarly', level=5)
     eevee = PokemonNormal('Eevee', level=5)
 
-    print(f"""Você tem 3 escolhas: 
+    print(Fore.CYAN + f"""Você tem 5 escolhas:
     1 - {pikachu}
     2 - {charmander}
     3 - {bulbasalro}
@@ -20,7 +23,7 @@ def escolher_pokemon_inicial(player):
     5 - {eevee}""")
     
     while True:
-        escolha = input("Escolha seu pokemon: ")
+        escolha = input(Fore.YELLOW + "Escolha seu pokemon: ")
 
         if escolha == '1':
             player.capturar(pikachu)
@@ -38,71 +41,86 @@ def escolher_pokemon_inicial(player):
             player.capturar(eevee)
             break
         else:
-            print('Escolha inválida...')
+            print(Fore.RED + 'Escolha inválida...')
 
 def salvar_game(player):
     try:
         with open('database.db', 'wb') as arquivo:
             pickle.dump(player, arquivo)
-            print("Jogo salvo com sucesso!")
+            print(Fore.GREEN + "Jogo salvo com sucesso!")
     except Exception as error:
-        print('Erro ao salvar game')
+        print(Fore.RED + 'Erro ao salvar game')
 
 def carregar_game():
     try:
         with open('database.db', 'rb') as arquivo:
             player = pickle.load(arquivo)
-            print('Load feito com sucesso')
+            print(Fore.GREEN + 'Load feito com sucesso')
             return player
     except Exception as error:
-        print('Save não encontrado')
+        print(Fore.RED + 'Save não encontrado')
+
+
 
 if __name__ == "__main__":
 
-    print('''
-          ============================================
-          Bem vindo ao jogo Pokemon RPG de terminal 2.
-          ============================================
+    print(Fore.MAGENTA + f'''
+          Bem-vindo ao jogo 
+
+            +=============================================================+
+            | ____   ___  _  _______ __  __  ___  _   _                   |
+            ||  _ \ / _ \| |/ / ____|  \/  |/ _ \| \ | |                  |
+            || |_) | | | | ' /|  _| | |\/| | | | |  \| |                  |
+            ||  __/| |_| | . \| |___| |  | | |_| | |\  |                  |
+            ||_|___ \___/|_|\_\_____|_|__|_|\___/|_|_\_| _       _   ___  |
+            ||_   _| ____|  _ \|  \/  |_ _| \ | |  / \  | |     / | / _ \ |
+            |  | | |  _| | |_) | |\/| || ||  \| | / _ \ | |     | || | | ||
+            |  | | | |___|  _ <| |  | || || |\  |/ ___ \| |___  | || |_| ||
+            |  |_| |_____|_| \_\_|  |_|___|_| \_/_/   \_\_____| |_(_)___/ |
+            +=============================================================+
+
           ''')
     
     player = carregar_game()
 
     if not player:
 
-        nome = input("Olá, eu sou o professor Marcos Vinícius. Qual o seu nome? \n")
+        nome = input(Fore.YELLOW + "Olá, eu sou o professor Marcos Vinícius. Qual o seu nome? \n")
         player = Player(nome)
 
-        print(f"Olá {player}, esse é o mundo pokemon, habitado por criaturas das mais diversas peculhariedades e a partir de agora sua missão é se tornar um mestre dos pokemons!")
+        print(Fore.CYAN + f"Olá {player}, esse é o mundo pokemon, habitado por criaturas das mais diversas peculiariedades e a partir de agora sua missão é se tornar um mestre dos pokemons!")
         print('Capture o máximo de pokemons que conseguir e lute com seus inimigos\n')
         player.mostrar_dinheiro()
 
         if player.pokemons:
-            print("Já vi que você tem alguns pokemons\n")
+            print(Fore.GREEN + "Já vi que você tem alguns pokemons\n")
             player.mostrar_pokemons()
         else:
-            print("Vejo que você não tem pokemons ainda, portando precisa escolher um: \n")
+            print(Fore.RED + "Vejo que você não tem pokemons ainda, portanto precisa escolher um:\n")
             escolher_pokemon_inicial(player)
 
-            print("Agora com pokemon em mãos, enfrente seu arqui-rival e colega Gary")
+            print(Fore.GREEN + "Agora com pokemon em mãos, enfrente seu arqui-rival e colega Gary")
             Gary = Inimigo(nome='Gary', pokemons=[PokemonAgua('Squirtle', level=4)])
             player.batalhar(Gary)
             salvar_game(player)
 
-            print("\n Sua jornada começa agora, seja bem vindo e divirta-se.\n")
+            print(Fore.MAGENTA + "\nSua jornada começa agora, seja bem-vindo e divirta-se.\n")
 
     while True:
-        print('''---------------------------------------
-    O que deseja fazer?
-              
-    1 - Explorar pelo mundo a fora
-    2 - Lutar com um inimigo
-    3 - Pokeagenda
-    0 - Sair do jogo
----------------------------------------\n''')
+        print(Fore.YELLOW + '''
+---------------------------------------
+O que deseja fazer?
+
+1 - Explorar pelo mundo afora
+2 - Lutar com um inimigo
+3 - Pokeagenda
+0 - Sair do jogo
+---------------------------------------
+''')
         
-        escolha = input("Sua escolha: ")
+        escolha = input(Fore.YELLOW + "Sua escolha: ")
         if escolha == "0":
-            print('Fechando jogo...')
+            print(Fore.CYAN + 'Fechando jogo...')
             break
         elif escolha == '1':
             player.explorar()
@@ -114,4 +132,4 @@ if __name__ == "__main__":
         elif escolha == '3':
             player.mostrar_pokemons()
         else:
-            print("Escolha inválida...")
+            print(Fore.RED + "Escolha inválida...")
